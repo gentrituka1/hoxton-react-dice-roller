594d6224d3ca446d4b2e359dcd1a18aba6435121
Gentrit Uka <gentrit-uka@live.com>
2022-08-18 18:41:14 +0200

    created a simplier version of the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
-import { RollDice } from './components/RollDice'
+
+
+
 
 function App() {
-  
+
+  const [die1, setDie1] = useState(0)
+  const [die2, setDie2] = useState(0)
+  const [status, setStatus] = useState('')
+
+  function rollDice() {
+    const die1 = Math.floor(Math.random() * 6) + 1
+    const die2 = Math.floor(Math.random() * 6) + 1
+    setDie1(die1)
+    setDie2(die2)
+    let diceTotal = die1 + die2
+    if (die1 === die2) {
+      setStatus('You rolled a double! You get a free turn!!')
+    } else {
+      setStatus(`You rolled ${diceTotal}`)
+    }
+  }
 
   return (
     <div className="App">
-      <RollDice />
+        <div className='dice-div'>
+          <div className='dice'>{die1}</div>
+          <div className='dice'>{die2}</div>
+        </div>
+        <button className='roll-dice' onClick={rollDice}>Roll Dice</button>
+        <h2>{status}</h2>
     </div>
   )
 }
